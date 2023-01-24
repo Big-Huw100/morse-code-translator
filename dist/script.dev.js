9eaaa7d88b031395866201153b26ba91cd804eff
Huw Williams <huwmorgan91@gmail.com>
2023-01-24 12:15:48 +0000

    Grabbed my elements

---
+++ dist/script.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// English to Morse Code Object
+var morseCode = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..',
+  " ": "/"
+}; // Grab my elements
+
+inputEnglish = document.getElementById("input");
+translateBtn = document.getElementById("button");
+translation = document.getElementById("translation");
---
+++ script.js
@@ -0,0 +1,37 @@
+// English to Morse Code Object
+const morseCode = {
+    A : '.-',
+    B : '-...',
+    C : '-.-.',
+    D : '-..',
+    E : '.',
+    F : '..-.',
+    G : '--.',
+    H : '....',
+    I : '..',
+    J : '.---',
+    K : '-.-',
+    L : '.-..',
+    M : '--',
+    N : '-.',
+    O : '---',
+    P : '.--.',
+    Q : '--.-',
+    R : '.-.',
+    S : '...',
+    T : '-',
+    U : '..-',
+    V : '...-',
+    W : '.--',
+    X : '-..-',
+    Y : '-.--',
+    Z : '--..',
+    " ": "/"
+ };
+
+
+ // Grab my elements
+
+inputEnglish = document.getElementById("input");
+translateBtn = document.getElementById("button");
+translation = document.getElementById("translation");
