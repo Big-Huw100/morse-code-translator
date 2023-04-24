2ccc17987c0dd096ee8f1c030cc88ddd02cbddd2
Huw Williams <huwmorgan91@gmail.com>
2023-04-24 15:50:44 +0100

    Morse to English now works

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -29,18 +29,61 @@ var engToMorse = {
   Y: '-.--',
   Z: '--..',
   " ": "/"
+}; // Morse to English Code Object
+
+var morseToEng = {
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "/": " "
 }; // Grab my elements
 
-inputEnglish = document.getElementById("english-input");
-morseTranslateBtn = document.getElementById("morse-button");
-morseTranslation = document.getElementById("morse-translation");
-inputMorse = document.getElementById("morse-input");
-engTranslateBtn = document.getElementById("english-button");
-engTranslation = document.getElementById("english-translation"); // Translate
+var inputEnglish = document.getElementById("english-input");
+var morseTranslateBtn = document.getElementById("morse-button");
+var morseTranslation = document.getElementById("morse-translation");
+var inputMorse = document.getElementById("morse-input");
+var engTranslateBtn = document.getElementById("english-button");
+var engTranslation = document.getElementById("english-translation"); // Translate English to Morse
 
 morseTranslateBtn.addEventListener("click", function (event) {
   event.preventDefault();
   morseTranslation.innerHTML = inputEnglish.value.toUpperCase().split("").map(function (el) {
-    return engToMorse[el] ? engToMorse[el] : el;
-  }).join("");
+    return engToMorse[el] ? engToMorse[el] : "";
+  }).join(" ");
+}); // Translate Morse to English
+
+engTranslateBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var morseWords = inputMorse.value.trim().split(" / ");
+  var engWords = morseWords.map(function (morseWord) {
+    var morseLetters = morseWord.split(" ");
+    var engLetters = morseLetters.map(function (morseLetter) {
+      return morseToEng[morseLetter] || "";
+    });
+    return engLetters.join("");
+  });
+  engTranslation.innerHTML = engWords.join(" ");
 });
--- script.js
+++ script.js
@@ -1,49 +1,92 @@
 // English to Morse Code Object
 const engToMorse = {
-    A : '.-',
-    B : '-...',
-    C : '-.-.',
-    D : '-..',
-    E : '.',
-    F : '..-.',
-    G : '--.',
-    H : '....',
-    I : '..',
-    J : '.---',
-    K : '-.-',
-    L : '.-..',
-    M : '--',
-    N : '-.',
-    O : '---',
-    P : '.--.',
-    Q : '--.-',
-    R : '.-.',
-    S : '...',
-    T : '-',
-    U : '..-',
-    V : '...-',
-    W : '.--',
-    X : '-..-',
-    Y : '-.--',
-    Z : '--..',
+    A: '.-',
+    B: '-...',
+    C: '-.-.',
+    D: '-..',
+    E: '.',
+    F: '..-.',
+    G: '--.',
+    H: '....',
+    I: '..',
+    J: '.---',
+    K: '-.-',
+    L: '.-..',
+    M: '--',
+    N: '-.',
+    O: '---',
+    P: '.--.',
+    Q: '--.-',
+    R: '.-.',
+    S: '...',
+    T: '-',
+    U: '..-',
+    V: '...-',
+    W: '.--',
+    X: '-..-',
+    Y: '-.--',
+    Z: '--..',
     " ": "/"
 };
 
+// Morse to English Code Object
+const morseToEng = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+    "/": " "
+};
+
 // Grab my elements
+const inputEnglish = document.getElementById("english-input");
+const morseTranslateBtn = document.getElementById("morse-button");
+const morseTranslation = document.getElementById("morse-translation");
 
-inputEnglish = document.getElementById("english-input");
-morseTranslateBtn = document.getElementById("morse-button");
-morseTranslation = document.getElementById("morse-translation");
+const inputMorse = document.getElementById("morse-input");
+const engTranslateBtn = document.getElementById("english-button");
+const engTranslation = document.getElementById("english-translation");
 
-inputMorse = document.getElementById("morse-input");
-engTranslateBtn = document.getElementById("english-button");
-engTranslation = document.getElementById("english-translation");
-
-// Translate
-
+// Translate English to Morse
 morseTranslateBtn.addEventListener("click", (event) => {
     event.preventDefault();
     morseTranslation.innerHTML = inputEnglish.value.toUpperCase().split("").map(el => {
-        return engToMorse[el] ? engToMorse[el] : el;
-    }).join("");
+        return engToMorse[el] ? engToMorse[el] : "";
+    }).join(" ");
+});
+
+// Translate Morse to English
+engTranslateBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const morseWords = inputMorse.value.trim().split(" / ");
+    const engWords = morseWords.map(morseWord => {
+        const morseLetters = morseWord.split(" ");
+        const engLetters = morseLetters.map(morseLetter => {
+            return morseToEng[morseLetter] || "";
+        });
+        return engLetters.join("");
+    });
+    engTranslation.innerHTML = engWords.join(" ");
 });
